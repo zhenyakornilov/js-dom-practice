865360e3e60830562d1562a607c77bf65ae87783
Yevhen Kornilov <zhenyakornilov98@gmail.com>
2022-08-11 23:19:26 +0300

    Create base files

---
+++ js/index.js
@@ -0,0 +1,5 @@
+import museumsData from "./data.js";
+console.log(museumsData[1]['description']);
+
+document.querySelector('.content').innerHTML = `<p>${museumsData[0]['description']}</p>`
+                                             
