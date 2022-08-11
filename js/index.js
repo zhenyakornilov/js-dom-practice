da56492e467c45d6da6ca3a0de10f539acd679c6
Yevhen Kornilov <zhenyakornilov98@gmail.com>
2022-08-12 00:45:54 +0300

    Work with design

--- js/index.js
+++ js/index.js
@@ -2,4 +2,4 @@ import museumsData from "./data.js";
 console.log(museumsData[1]['description']);
 
 document.querySelector('.content').innerHTML = `<p>${museumsData[0]['description']}</p>`
-                                             
+                                   
