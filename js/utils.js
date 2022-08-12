359189a4380d9333d3cf41decd03738689b2439a
Yevhen Kornilov <zhenyakornilov98@gmail.com>
2022-08-12 21:47:09 +0300

    work with dark mode

--- js/utils.js
+++ js/utils.js
@@ -2,7 +2,7 @@ function changeTheme() {
   
   const checkbox = document.getElementById("switcher");
   checkbox.addEventListener("change", () => {
-    const itemsClasses = [".body", ".header", ".footer", ".sidebar", ".content", ".museum-desc", ".nav-item"];
+    const itemsClasses = [".body", ".header", ".footer", ".sidebar", ".content", ".content-body"];
     itemsClasses.map((className) => {
         document.querySelector(className).classList.toggle("dark");
       });
